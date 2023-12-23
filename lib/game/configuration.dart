2fab281352c50b53a211303778fcfcbd124b991a
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2023-12-23 11:29:25 +0700

    up

---
+++ lib/game/configuration.dart
@@ -0,0 +1,8 @@
+class Config {
+  static const groundHeight = 110.0;
+  static const gameSpeed = 160.0;
+  static const pipeInterval = 1.5;
+  static const birdVelocity = 260;
+  static const gravity = -130.0;
+  static const cloudsHeight = 70.0;
+}
